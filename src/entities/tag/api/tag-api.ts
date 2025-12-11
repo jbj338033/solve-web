09c632c9ef7760a0f0f093bd929111456a39ccd2
jbj338033 <jbj338033@gmail.com>
2025-12-12 01:31:29 +0900

    refactor: rename tag api getAll to getTags

--- src/entities/tag/api/tag-api.ts
+++ src/entities/tag/api/tag-api.ts
@@ -2,5 +2,5 @@ import { api } from '@/shared/api'
 import type { Tag } from '../model/types'
 
 export const tagApi = {
-  getAll: () => api.get<Tag[]>('/tags'),
+  getTags: () => api.get<Tag[]>('/tags'),
 }
