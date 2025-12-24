142b31100ea0cf2b714689c6600f0c6c82caa264
jbj338033 <jbj338033@gmail.com>
2025-12-24 16:03:57 +0900

    feat: add problem source api

--- src/entities/problem/api/problem-api.ts
+++ src/entities/problem/api/problem-api.ts
@@ -45,19 +45,48 @@ export interface UpdateProblemRequest {
   isPublic?: boolean
 }
 
+export interface ProblemSourceRequest {
+  solutionCode: string
+  solutionLanguage: string
+  generatorCode?: string
+  generatorLanguage?: string
+}
+
+export interface ProblemSource {
+  solutionCode: string
+  solutionLanguage: string
+  generatorCode: string | null
+  generatorLanguage: string | null
+}
+
 export const problemApi = {
   getProblems: (params?: ProblemFilterParams) =>
     api.get<CursorPage<Problem>>('/problems', { params }),
+
+  getMyProblems: () =>
+    api.get<Problem[]>('/problems/my'),
+
+  getMyProblem: (problemId: number) =>
+    api.get<ProblemDetail>(`/problems/my/${problemId}`),
 
   getProblem: (problemId: number) =>
     api.get<ProblemDetail>(`/problems/${problemId}`),
 
   createProblem: (data: CreateProblemRequest) =>
-    api.post<ProblemDetail>('/problems', data),
+    api.post<{ id: number }>('/problems', data),
 
   updateProblem: (problemId: number, data: UpdateProblemRequest) =>
-    api.patch<ProblemDetail>(`/problems/${problemId}`, data),
+    api.patch<void>(`/problems/${problemId}`, data),
 
   deleteProblem: (problemId: number) =>
     api.delete<void>(`/problems/${problemId}`),
+
+  getSource: (problemId: number) =>
+    api.get<ProblemSource>(`/problems/my/${problemId}/source`),
+
+  saveSource: (problemId: number, data: ProblemSourceRequest) =>
+    api.put<void>(`/problems/my/${problemId}/source`, data),
+
+  deleteSource: (problemId: number) =>
+    api.delete<void>(`/problems/my/${problemId}/source`),
 }
--- src/entities/problem/index.ts
+++ src/entities/problem/index.ts
@@ -1,5 +1,11 @@
 export { problemApi } from './api/problem-api'
-export type { ProblemFilterParams, CreateProblemRequest, UpdateProblemRequest } from './api/problem-api'
+export type {
+  ProblemFilterParams,
+  CreateProblemRequest,
+  UpdateProblemRequest,
+  ProblemSourceRequest,
+  ProblemSource,
+} from './api/problem-api'
 export type { Problem, ProblemDetail, ProblemExample, ProblemType, ProblemDifficulty, ProblemSort } from './model/types'
 export { ProblemItem } from './ui/problem-item'
 export { ProblemItemSkeleton } from './ui/problem-item-skeleton'
